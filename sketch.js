52437177fee7dc1c9970889995845aed730cff40
swagenergypro <yuvraj6321@gmail.com>
2021-05-23 15:29:07 +0530

    hi
    '

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -33,8 +34,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    
 
     bird = new Bird(100,100);
+    log6 = new Log(230,180,80,PI/2)
+    chain = new Chain(bird.body,log6.body)
 
 }
 
@@ -61,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
 }
